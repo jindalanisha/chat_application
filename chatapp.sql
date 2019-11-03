4004fb413fd5a64ad4f173c337fb75f06b74ce1a
mansi2401 <45116907+mansi2401@users.noreply.github.com>
2019-11-03 17:47:19 +0530

    database added

---
+++ chatapp.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 03, 2019 at 01:14 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chatapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_pass` varchar(100) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_profile` varchar(255) NOT NULL,
+  `user_country` text NOT NULL,
+  `user_gender` text NOT NULL,
+  `forgotten_pass` varchar(100) NOT NULL,
+  `log_in` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_pass`, `user_email`, `user_profile`, `user_country`, `user_gender`, `forgotten_pass`, `log_in`) VALUES
+(1, 'mansi', 'toshi', 'mansi@gmail.com', '', 'india', 'f', '', 'Online');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_chat`
+--
+
+CREATE TABLE `users_chat` (
+  `msg_id` int(11) NOT NULL,
+  `sender_username` varchar(100) NOT NULL,
+  `receiver_username` varchar(100) NOT NULL,
+  `msg_content` varchar(255) NOT NULL,
+  `msg_status` text NOT NULL,
+  `msg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_chat`
+--
+
+INSERT INTO `users_chat` (`msg_id`, `sender_username`, `receiver_username`, `msg_content`, `msg_status`, `msg_date`) VALUES
+(0, 'mansi', 'mansi', 'hi', 'read', '2019-11-01 15:58:13'),
+(0, 'mansi', '', 'hey', 'unread', '2019-11-01 16:12:38'),
+(0, 'mansi', '', 'hello', 'unread', '2019-11-01 16:14:09');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
